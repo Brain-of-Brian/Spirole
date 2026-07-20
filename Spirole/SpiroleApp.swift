0bdf5cb23378de8ef1797008a2e2f70ff0aa34e2
Brain-of-Brian <brianmo3530+github@gmail.com>
2026-07-20 01:41:37 -0500

    Initial Commit

---
+++ Spirole/SpiroleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiroleApp.swift
+//  Spirole
+//
+//  Created by 14 BGCCLoan Lbrary on 7/20/26.
+//
+
+import SwiftUI
+
+@main
+struct SpiroleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
